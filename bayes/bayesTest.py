39cb39a8e377ad9998fb58c1c3ecd876f2da5375
Alfred <alfred_bit@sina.cn>
2017-09-20 20:35:11 +0800

    E:/Program Files (x86)/Git/Git/bayes/bayes.py bayesTest.py edited - 2nd

--- bayes/bayesTest.py
+++ bayes/bayesTest.py
@@ -1,8 +1,26 @@
 
 # bayesTest.py
 
+import bayes
 
+listOPosts, listClasses = bayes.loadDataSet()
 
+myVocabList = bayes.createVocabList(listOPosts)
+
+# print(myVocabList)
+
+# print(bayes.setOfWords2Vec(myVocabList, listOPosts[0]))
+# print(bayes.setOfWords2Vec(myVocabList, listOPosts[3]))
+
+trainMat = []
+for postinDoc in listOPosts:
+	trainMat.append(bayes.setOfWords2Vec(myVocabList, postinDoc))
+
+p0V, p1V, pAb = bayes.trainNB0(trainMat, listClasses)
+
+print('p0V = ', p0V)
+print('p1V = ', p1V)
+print('pAb = ', pAb)
 
 
 
