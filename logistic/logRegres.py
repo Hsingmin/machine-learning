7555262ffdb6e7f8e92fe30fe60c51c8d0237be7
Alfred <alfred_bit@sina.cn>
2017-09-25 14:31:52 +0800

    E:/Program Files (x86)/Git/Git/logistic/logRegres.py and logRgressTest.py edited to be executable

--- logistic/logRegres.py
+++ logistic/logRegres.py
@@ -1,6 +1,8 @@
 
 # logRegres.py
 
+from numpy import *
+
 def loadDataSet():
 	dataMat = []; labelMat = []
 
@@ -70,7 +72,7 @@ def stockGradAscent1(dataMatrix, classLabels, numIter = 150):
 	m, n = shape(dataMatrix)
 	weights = ones(n)
 	for j in range(numIter):
-		dataIndex = range(m)
+		dataIndex = list(range(m))
 		
 		for i in range(m):
 			alpha = 4 / (1.0 + j + i) + 0.01
--- logistic/logRegressTest.py
+++ logistic/logRegressTest.py
@@ -3,18 +3,23 @@
 
 import logRegres
 
+from numpy import *
+
 dataArray, labelMat = logRegres.loadDataSet()
 
 # print('dataArray : ', dataArray)
 # print('labelArray : ', labelMat)
 
-weights = (logRegres.gradAscent(dataArray, labelMat))
+# weights = (logRegres.gradAscent(dataArray, labelMat))
+# logRegres.plotBestFit(weights.getA())
 
-logRegres.plotBestFit(weights.getA())
+# weights = (logRegres.stockGradAscent0(array(dataArray), labelMat))
+#logRegres.plotBestFit(weights)
 
+# weights = (logRegres.stockGradAscent1(array(dataArray), labelMat))
+# logRegres.plotBestFit(weights)
 
-
-
+logRegres.multiTest()
 
 
 
