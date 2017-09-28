cf3b1603a47542a1c11c68a4a421988d2af8eff0
Alfred <alfred_bit@sina.cn>
2017-09-28 13:54:14 +0800

    d:/python_work/machine-learning/apriori/apriori.py edited

--- apriori/apriori.py
+++ apriori/apriori.py
@@ -89,6 +89,58 @@ def rulesFromConseq(freqSet, H, supportData, br1, minConf = 0.7):
 		if(len(Hmp1) > 1):
 			rulesFromConseq(freqSet, Hmp1, supportData, br1, minConf)
 
+from time import sleep
+from votesmart import votesmart
+votesmart.apikey = '49024thereoncewasamanfromnantucket94040'
+def getActionIds():
+	actionIdList = []; billTitleList = []
+	fr = open('recent20bills.txt')
+	for line in fr.readlines():
+		billNum = int(line.split('\t')[0])
+		try:
+			billDetail = votesmart.votes.getBill(billNum)
+			for action in billDetail.actions:
+				if action.level == 'House' and \
+					(action.stage == 'Passage' or \
+					action.stage == 'Amendment Vote'):
+					actionId = int(action.actionId)
+					print('bill : %d has actionId : %d' %(billNum, actionId))
+					actionIdList.append(actionId)
+					billTitleList.append(line.strip().split('\t')[1])
+		except:
+			print('problem getting bill %d' %billNum)
+		sleep(1)
+	return actionIdList, billTitleList
+
+def getTransList(actionIdList, billTitleList):
+	itemMeaning = ['Republican', 'Democratic']
+	for billTitle in billTitleList:
+		itemMeaning.append('%s -- Nay' % billTitle)
+		itemMeaning.append('%s -- Yea' % billTitle)
+	transDict = {}
+	voteCount = 2
+	for actionId in actionIdList:
+		sleep(3)
+		print('getting votes for actionId : %d' %actionId)
+		try:
+			voteList = votesmart.votes.getBillActionVotes(actionId)
+			for vote in voteList:
+				if not transDict.has_key(vote.candidateName):
+					transDict[vote.candidateName] = []
+					if vote.officeParties == 'Democratic':
+						transDict[vote.candidateName].append(1)
+					elif vote.officeParties == 'Republican':
+						transDict[vote.candidateName].append(0)
+				if vote.action == 'Nay':
+					transDict[vote.candidateName].append(voteCount)
+				elif vote.action == 'Yea':
+					transDict[vote.candidateName].append(voteCount + 1)
+
+		except:
+			print('problem getting actionId: %d' %actionId)
+		voteCount += 2
+	return transDict, itemMeaning
+
 
 
 
