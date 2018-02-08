47ddee95f80cc9261c22721d125a4c52aab8c002
Alfred <alfred_bit@sina.cn>
2018-02-09 00:41:47 +0800

    E:/Program Files (x86)/Git/Git/BPTT RNN added backforward propagate on time

--- tensorflow-tutorial/rnn_in_practice/bptt.py
+++ tensorflow-tutorial/rnn_in_practice/bptt.py
@@ -34,8 +34,46 @@ class SIMPLE_RNN:
 
 		return np.array(output)
 
-#
-class BPTT_RNN:
+# RNN network with BPTT .
+class BPTT_RNN(SIMPLE_RNN):
+	# Sigmoid activate function .
+	def activate(self, x):
+		return 1 / (1 + np.exp(-1))
+
+	# Softmax converse function .
+	def transform(self, x):
+		safe_exp = np.exp(x - np.max(x))
+		return safe_exp / np.sum(safe_exp)
+
+	def bptt(self, x, y):
+		x, y, n = np.asarray(x), np.asarray(y), len(y)
+
+		# Get outputs and calculate State .
+		o = self.run(x)
+		dis = o - y
+		# dv = (o - y) * state .
+		dv = dis.T.dot(self.state[:-1])
+		du = np.zeros_like(self.u)
+		dw = np.zeros_like(self.w)
+
+		# From moment n-1 to -1
+		for t in range(n-1, -1, -1):
+			st = self.state[t]
+			ds = self.v.T.dot(dis[t]) * st * (1 - st)
+			#
+			for bptt_step in range(t, max(-1, t-10), -1):
+				du += np.outer(ds, x[bptt_step])
+				dw += np.outer(ds, self.state[bptt_step])
+				st = self.state[bptt_step-1]
+				ds = self.w.T.dot(ds) * st * (1-st)
+
+		return du, dv, dw
+
+	def loss(self, x, y):
+		o = self.run(x)
+		return np.sum(
+				-y * np.log(np.maximum(o, 1e-12))
+				- (1-y) * np.log(np.maximum(1-o, 1e-12)))
 
 
 def main(argv=None):
