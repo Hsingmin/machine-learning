cd1bb22d8bc0f01af0a4c175e3cb8c809ca47947
Alfred <alfred_bit@sina.cn>
2018-03-19 17:28:03 +0800

    CNN added

--- tensorflow-tutorial/tf_imageprocess/image_to_tfrecord.py.py
+++ tensorflow-tutorial/tf_imageprocess/image_to_tfrecord.py.py
@@ -6,13 +6,9 @@
 #		numpy 1.13.1
 # -*- author: Hsingmin Lee
 #
-# inception_migration.py -- Migration learning with Inception-v3 model .
+# image_to_tfrecord.py -- Convert images into tf.train.Example Protocol Buffer
+# format stored in TFRecord files.
 #
-# Regard the former layers except the last full-connected layer as 
-# a whole the layer called bottleneck .
-# Bottleneck is used to extract image features for the new single
-# full-connected layer that needed to be trained to solve another
-# classification problem .
 
 import glob
 import os.path
@@ -53,6 +49,49 @@ TEST_PERCENTAGE = 10
 LEARNING_RATE = 0.01
 STEPS = 4000
 BATCH = 100
+# Cast image features into tfrecord features .
+def _int64_feature(value):
+	return tf.train.Feature(int64_list = tf.train.Int64List(value=[value]))
+
+def _bytes_feature(value):
+	return tf.train.Feature(bytes_list = tf.train.BytesList(value=[value]))
+
+def _float_feature(value):
+	return tf.train.Feature(float_list = tf.train.FloatList(value=[value]))
+
+# Convert single image into tfrecord .
+def convert_to_tfrecord(image, n_classes, label_index, tfrecord):
+	image_raw = tf.gfile.FastGFile(image, 'rb').read()
+	
+	image_label = np.zeros(n_classes, dtype=np.float32)
+	image_label[label_index] = 1
+
+	length = image_data.shape[0]
+	width = image_data.shape[1]
+	channel = image_data.shape[2]
+	
+	writer = tf.python_io.TFRecordWriter(tfrecord)
+	# Convert into Example Protocol Buffer .
+	example = tf.train.Example(features=tf.train.Feature(features={
+		'length': _int64_feature(length),
+		'width': _int64_feature(width),
+		'channel': _int64_feature(channel),
+		'image_raw': _bytes_feature(image_raw),
+		'label': _int64_feature(label)
+		}))
+
+	writer.write(example.SerializedToString(example))
+
+	writer.close()
+
+def parse_from_tfrecord(example):
+	return tf.parse_single_example(
+			example, features={
+				'length': tf.FixedLenFeature([], tf.int64),
+				'width': tf.FixedLenFeature([], tf.int64),
+				'channel': tf.FixedLenFeature([], tf.int64),
+				'image_raw': tf.FixedLenFeature([], tf.string),
+				'label': tf.FixedLenFeature([], tf.int64)})
 
 # Dispatch training dataset , validation dataset and testing dataset
 # from image file .
@@ -72,6 +111,7 @@ def create_image_lists(testing_percentage, validation_percentage):
 	#           './data/flower_photos/sunflowers',
 	#           './data/flower_photos/tulips']
 	
+	# The first element in list is root directory .
 	is_root_dir = True
 	for sub_dir in sub_dirs:
 		if is_root_dir:
