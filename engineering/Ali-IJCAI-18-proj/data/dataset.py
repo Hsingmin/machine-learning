24a99ded77fc138dd67b72f875abc7674b45e69f
Alfred <alfred_bit@sina.cn>
2018-04-27 23:56:41 +0800

    feature dispatch

--- engineering/Ali-IJCAI-18-proj/data/dataset.py
+++ engineering/Ali-IJCAI-18-proj/data/dataset.py
@@ -33,6 +33,7 @@ TEST_DATASET_RAW = 'round1_ijcai_18_test_a_20180301.txt'
 TRAIN_DATASET_CSV = 'ijcai_18_train_dataset.csv'
 TEST_DATASET_CSV = 'ijcai_18_test_dataset.csv'
 
+# Convert Unix time stamps into local date. 
 def ustamps_to_time(value):
     format = '%Y-%m-%d %H:%M:%S'
     # Get local time.
@@ -40,16 +41,157 @@ def ustamps_to_time(value):
     # Format local time like 2018-04-26 22:41:29 . 
     date = time.strftime(format, value)
     return date
+# Encode user_occupation_id feature.
+def user_occupation_encode(x):
+    if x == -1 | x == 2003:
+        return 1
+    elif x == 2002:
+        return 2
+    else:
+        return 3
 
-def feature_encode(data):
+# Encode user_star_level feature.
+def user_starlevel_encode(x):
+    if x == -1 | x == 3000:
+        return 1
+    elif x == 3009 | x == 3010:
+        return 2
+    else:
+        return 3
+
+# Encode shop_score_delivery feature.
+def shop_delivery_encode(x):
+    if x == -5:
+        z = 1
+    else:
+        z = (x-4.1)*10 + 1
+
+    if (z >= 2) & (z <= 4):
+        return 1
+    elif (z >= 5) & (z <= 7):
+        return 2
+    else:
+        return 3
+
+# Encode shop_score_service feature.
+def shop_service_encode(x):
+    if x == -5:
+        z = 1
+    else:
+        z = (x-3.93)*10 + 1
+
+    if (z >= 2) & (z <= 12):
+        return 1
+    elif (z >= 13) & (z <= 15):
+        return 2
+    else:
+        return 3
+
+# Encode shop_score_description feature.
+def shop_description_encode(x):
+    if x == -5:
+        z = 1
+    else:
+        z = (x-3.93)*10 + 1
+
+    if (z >= 2) & (z <= 12):
+        return 1
+    elif (z >= 13) & (z <= 15):
+        return 2
+    else:
+        return 3
+
+# Encode shop_review_positive_rate feature.
+def shop_reviewrate_encode(x):
+    if x == -1:
+        z = 1
+    else:
+        z = (x-0.714)*50 + 1
+
+    if (z >= 2) & (z <= 12):
+        return 1
+    elif (z >= 13) & (z <= 15):
+        return 2
+    else:
+        return 3
+
+# Dataset fields and features preprocess.
+def feature_process(data):
     le = preprocessing.LabelEncoder()
 
+    # Item field label encode. 
     data['len_item_category'] = data['item_category_list'].map(lambda x: len(str(x).split(';')))
     data['len_item_property'] = data['item_property_list'].map(lambda x: len(str(x).split(';')))
 
     for i in range(1, 3):
-        data['item_category_list' + str(i)] = lbl.fit_tranform(data['item_category_list'].map(
+        data['item_category_list' + str(i)] = le.fit_tranform(data['item_category_list'].map(
             lambda x: str(str(x).split(';')[i]) if len(str(x).split(';')) > i else ''))
+
+    for i in range(10):
+        data['item_property_list' + str(i)] = le.fit_transform(data['item_property_list'].map(
+            lambda x: str(str(x).split(';')[i]) if len(str(x).split(';')) > i else ''))
+    for col in ['item_id', 'item_brand_id', 'item_city_id']:
+        data[col] = le.fit_transform(data[col])
+
+    # User field label encode.
+    for col in ['user_id']:
+        data[col] = le.fit_tranform(data[col])
+    data['gender0'] = data['user_gender_id'].apply(lambda x: 1 if x==-1 else 2)
+    data['age0'] = data['user_age_level'].apply(lambda x: 1 if x==1000|x==1001|x==-1 else 2)
+    data['occupation0'] = data['user_occupation_id'].apply(user_occupation_encode)
+    data['star0'] = data['user_star_level'].apply(user_starlevel_encode)
+
+    # Context field encode.
+    data['realtime'] = data['context_timestamp'].apply(ustamps_to_time)
+    data['realtime'] = pd.to_datetime(data['realtime'])
+    data['day'] = data['realtime'].dt.day
+    data['hour'] = data['realtime'].dt.hour
+
+    data['len_predict_category_property'] = data['predict_category_property'].map(
+            lambda x: len(str(x).split(';')))
+    for i in range(5):
+        data['predict_category_property' + str(i)] = le.fit_transform(data['predict_category_property'].map(
+                lambda x: str(str(x).split(';')[i]) if len(str(x).split(';')) > i else ''))
+
+    data['context_page0'] = data['context_page_id'].apply(
+            lambda x: 1 if x==4001 | x==4002 | x==4003 | x==4004 | x==4007 else 2)
+
+    # Shop filed encode.
+    for col in ['shop_id']:
+        data[col] = le.fit_transform(data[col])
+    data['shop_score_delivery0'] = data['shop_score_delivery'].apply(
+            lambda x: 0 if 0.98 >= x >= 0.96 else 1)
+
+    return data
+
+# Split shop fields and features into segments.
+def split_shop_feature(data):
+    data['shop_score_delivery'] = data['shop_score_delivery']*5
+    data = data[data['shop_score_delivery'] != -5]
+    data['shop_delivery_segment'] = data['shop_score_delivery'].apply(shop_delivery_encode)
+    delivery_segment_counts = data.shop_delivery_segment.value_counts()
+
+    data['shop_score_service'] = data['shop_score_service']*5
+    data = data[data['shop_score_service'] != -5]
+    data['shop_service_segment'] = data['shop_score_service'].apply(shop_service_encode)
+    service_segment_counts = data.shop_service_segment.value_counts()
+
+    data['shop_score_description'] = data['shop_score_description']*5
+    data = data[data['shop_score_description'] != -5]
+    data['shop_description_segment'] = data['shop_score_description'].apply(shop_description_encode)
+    description_segment_counts = data.shop_description_segment.value_counts()
+
+    data = data[data['shop_review_positive_rate'] != -1]
+    data['shop_reviewrate_segment'] = data['shop_review_positive_rate'].apply(shop_reviewrate_encode)
+    reviewrate_segment_counts = data.shop_reviewrate_segment.value_counts()
+
+    data['shop_field'] = data.apply(lambda x: 1 if (x.shop_delivery_segment == 3) & (x.shop_description_segment == 3)
+            & (x.shop_reviewrate_segment == 3) & (x.shop_service_segment == 3) else 0, axis=1)
+    del data['shop_delivery_segment']
+    del data['shop_description_segment']
+    del data['shop_reviewrate_segment']
+    del data['shop_service_segment']
+    return data
 
 # Get columns header keywords and write into file.
 # args:
